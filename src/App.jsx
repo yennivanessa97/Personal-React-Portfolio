5e4b5e36b69707af335b44881dbb9556fe6f0e07
Yenni Delgado <yennivanessadelgado@gmail.com>
2025-12-15 17:32:53 -0500

    Testing the global email and name

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
     return (
         <main>
         <div>
-            <h1>Welcome to my portfolio</h1>
+            <h1>Welcome to my portfolio with React</h1>
         </div>
         </main>
         
