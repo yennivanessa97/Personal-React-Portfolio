a025ba5928528a5df791147b301fed3ad3a7a98b
Diego Ferro <diego.ferro@gmail.com>
2025-12-15 17:24:34 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+        <div>
+            <h1>Welcome to my portfolio</h1>
+        </div>
+        </main>
+        
+    )
+}
+
+export default App
