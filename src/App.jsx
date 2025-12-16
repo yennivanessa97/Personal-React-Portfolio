edc8f3789d9d208bd9f77aa676963e8234142d27
Yenni Delgado <yennivanessadelgado@gmail.com>
2025-12-16 12:08:15 -0500

    Testing tailwindcss and add public folder

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
     return (
         <main>
         <div>
-            <h1>Welcome to my portfolio with React</h1>
+            <h1 className= "text-3xl underline">Welcome to my portfolio with React</h1>
         </div>
         </main>
         
